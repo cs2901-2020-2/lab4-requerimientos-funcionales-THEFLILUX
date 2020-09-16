be39fc7ae376790bfee9a5ac1765907bf644def7
THEFLILUX <jorge.nicho@utec.edu.pe>
2020-09-16 09:45:55 -0500

    avance

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -1,5 +1,6 @@
 package cs.lab;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -11,6 +12,32 @@ public class  DNASequencer {
     }
 
     public String calculate(List<String> part){
+        String secuencia_adn1="AGATTA";
+        String secuencia_adn2="GATTACA";
+        String secuencia_adn3="TACAGA";
+        String common = secuencia_adn1;
+
+//        HashSet<Character> h1 = new HashSet<Character>();
+//        HashSet<Character> h2 = new HashSet<Character>();
+//
+//        for(int i=0; i<secuencia_adn1.length();i++){
+//            h1.add(secuencia_adn1.charAt(i));
+//        }
+//
+//        for(int i=0; i<secuencia_adn2.length();i++){
+//            h2.add(secuencia_adn1.charAt(i));
+//        }
+
+        for(int i=0;i<secuencia_adn2.length();i++){
+            for(int j=0;j<secuencia_adn1.length();j++){
+                if(secuencia_adn2.charAt(i)!=secuencia_adn1.charAt(j)){
+                    common += secuencia_adn2.charAt(i);
+                    break;
+                }
+            }
+        }
+        System.out.println("common is: "+common);
+
         return "AGATTACAGA";
     }
 }
